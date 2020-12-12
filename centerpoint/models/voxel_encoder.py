d17f086508136ab8eee7d483f06f6b4540322bae
John Lambert <johnwl@stanford.edu>
2020-12-12 18:27:10 -0500

    add docstring and typehints to VoxelFeatureExtractorV3

--- centerpoint/models/voxel_encoder.py
+++ centerpoint/models/voxel_encoder.py
@@ -10,13 +10,33 @@ from torch.nn import functional as F
 
 class VoxelFeatureExtractorV3(nn.Module):
     def __init__(
-        self, num_input_features=4, norm_cfg=None, name="VoxelFeatureExtractorV3"
-    ):
+        self,
+        num_input_features: int = 4,
+        norm_cfg = None,
+        name: str = "VoxelFeatureExtractorV3"
+    ) -> None:
+        """
+        """
         super(VoxelFeatureExtractorV3, self).__init__()
         self.name = name
         self.num_input_features = num_input_features
 
-    def forward(self, features, num_voxels, coors=None):
+    def forward(
+        self,
+        features: torch.Tensor,
+        num_voxels: torch.Tensor,
+        coors=None
+    ) -> torch.Tensor:
+        """
+        Args:
+            features: tensor of shape [360000, 10, 5], zero-padded where less
+                than 10 points per voxel
+            num_voxels: tensor of shape [360000,] representing #pts / voxel
+
+        Returns:
+            points_mean: tensor of shape [360000, 5] representing mean of
+                all points within the voxel
+        """
         points_mean = features[:, :, : self.num_input_features].sum(
             dim=1, keepdim=False
         ) / num_voxels.type_as(features).view(-1, 1)
