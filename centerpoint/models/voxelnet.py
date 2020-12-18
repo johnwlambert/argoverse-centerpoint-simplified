0c547420e48c5dfec1fd9463f53eadfe75c22e42
John Lambert <johnlambert@gatech.edu>
2020-12-17 21:59:05 -0500

    add type annotations and discussion of network output shapes

--- centerpoint/models/voxelnet.py
+++ centerpoint/models/voxelnet.py
@@ -28,7 +28,17 @@ class VoxelNet(SingleStageDetector):
 
         return x
 
-    def forward(self, example, return_loss=True, **kwargs):
+    def forward(self, example: Dict[str,Any], return_loss: bool = True, **kwargs):
+        """
+        Args:
+            example is a dictionary with keys like
+            dict_keys(['metadata', 'points', 'voxels', 'shape', 'num_points', 'num_voxels', 'coordinates', 'annos'])
+        
+        `voxels` could be a tensor of shape [150031, 10, 5]
+        `coordinates` could be a tensor of shape [150031, 4], with entries like [0, 20, 720, 698]
+        `num_points_in_voxel` could be a tensor of shape [150031] with entries like [10]
+        `num_voxels` could be a tensor of shape [4], with entries like [37507, 37508, 37508, 37508]
+        """
         voxels = example["voxels"]
         coordinates = example["coordinates"]
         num_points_in_voxel = example["num_points"]
@@ -44,7 +54,19 @@ class VoxelNet(SingleStageDetector):
             input_shape=example["shape"][0],
         )
 
+        # x has shape [4, 512, 180, 180], 8x downsampled from 1440 x 1440
         x = self.extract_feat(data)
+        
+        # get out list of length 6 (for 6 tasks)
+        # each is a dictionary with keys
+        #      dict_keys(['reg', 'height', 'dim', 'rot', 'vel', 'hm'])
+        #      values could have shape
+        #           height: [4, 1, 180, 180]
+        #           dim: [4, 3, 180, 180]
+        #           rot: [4, 2, 180, 180]
+        #           vel: [4, 2, 180, 180]
+        #           hm: [4, 1, 180, 180]
+        #           reg: [4, 2, 180, 180]
         preds = self.bbox_head(x)
 
         if return_loss:
