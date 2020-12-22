2eb1d1c94c9087b89129c3560bd01d1cdf1d0212
John Lambert <johnlambert@gatech.edu>
2020-12-22 00:32:38 -0500

    Update plot_centerpoint_detections.py

--- plot_centerpoint_detections.py
+++ plot_centerpoint_detections.py
@@ -284,7 +284,6 @@ def main():
     argoverse_root = "/home/ubuntu/argoverse/argoverse-tracking/val"
     
     for token, sweep_output in pkl_data.items():
-        pdb.set_trace()
         lidar_subpath = sweep_output['metadata']["token"]
         log_id = lidar_subpath.split('/')[0]
         lidar_fpath = f'{argoverse_root}/{lidar_subpath}'
